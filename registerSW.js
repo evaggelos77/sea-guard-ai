a1a91a2bda74a3dafb91ff2a0ef1dabc97a490b6
evaggelos77 <events.topolytexno@gmail.com>
2026-06-27 23:14:07 +0300

    Deploy: custom domain + copy fixes

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sea-guard-ai/sw.js', { scope: '/sea-guard-ai/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
